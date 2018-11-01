1af429099eebe9e59fed8c0ac59de9ad8588e205
Michael <mjmjulien10@gmail.com>
2018-11-01 09:47:53 -0600

    created createStickFigure() Method, drawTurtle() method, and Hat Method

---
+++ drawingProject/View/DrawingView.swift
@@ -0,0 +1,81 @@
+//
+//  DrawingView.swift
+//  drawingProject
+//
+//  Created by Julien, Michael on 11/1/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class DrawingView: UIView
+{
+
+    
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) -> Void
+    {
+     
+     // Drawing code
+        createStickFigure().stroke()
+        drawTurtle().stroke()
+        drawHat().stroke()
+    }
+    
+    private func createStickFigure() -> UIBezierPath
+    {
+        let figure : UIBezierPath = UIBezierPath()
+        
+        UIColor.blue.setStroke()
+        figure.lineWidth = 8.0
+       
+        figure.addArc(withCenter: CGPoint(x: 200, y: 200),
+                      radius: CGFloat(20),
+                      startAngle: CGFloat(0),
+                      endAngle: CGFloat(2) * CGFloat.pi,
+                      clockwise: true)
+        
+        figure.move(to: CGPoint(x: 200, y: 220))
+        figure.addLine(to: CGPoint(x: 200, y: 270))
+        figure.move(to: CGPoint(x: 180, y: 240))
+        figure.addLine(to: CGPoint(x: 220, y: 240))
+        figure.move(to: CGPoint(x: 200, y: 270))
+        figure.addLine(to: CGPoint(x: 180, y: 300))
+        figure.move(to: CGPoint(x: 200, y: 270))
+        figure.addLine(to: CGPoint(x: 220, y: 300))
+        
+        return figure
+    }
+    
+    private func drawTurtle() -> UIBezierPath
+{
+        
+    let logo : UIBezierPath = UIBezierPath()
+        UIColor.white.setFill()
+        logo.move(to: CGPoint(x: 50, y: 250))
+        logo.addLine(to: CGPoint(x: 100, y: 300))
+        logo.addLine(to: CGPoint(x: 50, y: 350))
+        logo.close()
+        logo.fill()
+        
+        return logo
+    }
+    /*
+     //HAT METHOD
+    private func drawHat() -> UIBezierPath
+    {
+        let hat : UIBezierPath = UIBezierPath()
+        UIColor.blue.setStroke()
+        hat.move(to: CGPoint(x: 440, y: 550))
+        hat.addLine(to: CGPoint(x:350, y: 550))
+        
+        
+        hat.close()
+        hat.fill()
+        
+        return hat
+     */
+    }
+
+}
